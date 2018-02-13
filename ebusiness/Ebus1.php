b690b8dc3c614514b361c6ff691b554b27beb5c1
Leo Cheung <117326763@umail.ucc.ie>
2018-02-13 20:37:33 +0000

    update

--- ebusiness/Ebus1.php
+++ ebusiness/Ebus1.php
@@ -1,36 +1,33 @@
 <!DOCTYPE html>
 <html>
     <head>
-        
-    <title>Select Product</title>
-    <link rel ="stylesheet" href="mystylesheet.css" type ="text/css"/>
-    
-    
-    <!--jQuery-->
-    <script src ="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script type= "text/javascript" src = "cost_calc.js"></script>
+        <title>Select Product</title>
+        <link rel ="stylesheet" href="mystylesheet.css" type ="text/css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+            
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <!--jQuery-->
+        <script src ="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script type= "text/javascript" src = "cost_calc.js"></script>
     </head>
     
     <style>
-        .formstyling{
-            padding-left: 25%;
-        }
         
         h1{
             text-align:center;
             background-color: #00001a;
-             color: #ffffff
+            color: #ffffff
         }
-        
-        
-        
     </style>
+    
     <body>
         <h1>Select a Product</h1>
         
         <br/>
     
-       <div class="formstyling">        
+       <div class="mx-auto" style="width: 250px;">        
         <form method="POST" action ="Ebus2.php">
             <label for ="salesforce">
                 <input type ="radio" id ="salesforce" name="product" checked onClick="disablebtnProcess()"/>
@@ -65,8 +62,8 @@
             
         </form>
         
-        <button id ="calculatecost" ;style="border-radius: 8px;" style ="text-color: white;" onClick="calcSub()">Calculate Cost</button>
-        <a role="button"  href="Ebus1.php">Clear Choice</a>
+            <button id ="calculatecost" ;style="border-radius: 8px;" style ="text-color: white;" onClick="calcSub()">Calculate Cost</button>
+            <a role="button"  href="Ebus1.php">Clear Choice</a>
         </div>
         <br/>
         
