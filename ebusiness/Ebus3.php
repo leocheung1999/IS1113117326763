057c8b517c60dc50d9fdde5eaed0a66655fa305e
Leo Cheung <117326763@umail.ucc.ie>
2018-02-25 21:08:15 +0000

    fixing

--- ebusiness/Ebus3.php
+++ ebusiness/Ebus3.php
@@ -8,11 +8,18 @@ session_start();
     </head>
     <body>
         <h4>RECEIPT</h4>
+        
+        <!--Set session variables-->
+        <?php
+        $_SESSION["name"]= $_POST["name"];
+        $_SESSION["email"]= $_POST["email"];
+        ?>
                 
         <?php
         // Echo session variables that were set on previous page
-        echo "Name :" . $_SESSION["name"] . ".";
-        echo "Total is " . $_SESSION["total"] . ".";
+        echo "Name:" . $_SESSION["name"],"<br/>";
+        echo "Email:" . $_SESSION["email"] . " . ","<br/>";
+        echo "Total Price: " . $_SESSION["total"] . " . ";
         
          
         ?>
