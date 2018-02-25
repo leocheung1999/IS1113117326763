b8fb14001e3ce8a9c819d236b2b4003571aa2e3a
Leo Cheung <117326763@umail.ucc.ie>
2018-02-25 20:00:01 +0000

    calc

--- ebusiness/cost_calc.js
+++ ebusiness/cost_calc.js
@@ -36,10 +36,10 @@ function calcSub(){
     
     function display(parm1 , parm2 , parm3 , parm4){
         
-        document.getElementById("subtotal").value = $ + parm1;
-        document.getElementById("discount").value = $ + parm2;
-        document.getElementById("vat").value = $ + parm3;
-        document.getElementById("total").value = $ + parm4;
+        document.getElementById("subtotal").value = '$' + parm1;
+        document.getElementById("discount").value = '$' + parm2;
+        document.getElementById("vat").value = '$' + parm3;
+        document.getElementById("total").value = '$' + parm4;
         
         enablebtnProceed();
     }
