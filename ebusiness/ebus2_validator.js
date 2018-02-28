905ac73c7c039f329bfa7653010297011ec460bb
Leo Cheung <117326763@umail.ucc.ie>
2018-02-28 13:59:01 +0000

    fix

--- ebusiness/ebus2_validator.js
+++ ebusiness/ebus2_validator.js
@@ -14,14 +14,22 @@ function validateDetails(){
     
     if(pin ==""){
         alert("Please enter your PIN");
-        
+    }
+    else{
+        enablebtnPurchase();
     }
     if (String(pin).length < 4){
         alert("Please make sure your PIN is accurate");
     }
+     else{
+        enablebtnPurchase();
+    }
     
      if (email == ""){
         alert("Please ensure you have entered your E-mail.");
+    }
+     else{
+        enablebtnPurchase();
     }
     
     if (name == ""){
